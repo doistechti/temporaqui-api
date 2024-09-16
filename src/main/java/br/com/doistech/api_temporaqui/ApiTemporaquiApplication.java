652d2f6ff3fe3f3cc1eedebafe557fc564af4da3
Brenno Agostini <brenno.agostini@ipremios.com>
2024-09-15 22:06:07 -0300

    Inicializando o projeto
    Inicializando o projeto

---
+++ src/main/java/br/com/doistech/api_temporaqui/ApiTemporaquiApplication.java
@@ -0,0 +1,13 @@
+package br.com.doistech.api_temporaqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTemporaquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTemporaquiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/doistech/api_temporaqui/ApiTemporaquiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.doistech.api_temporaqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTemporaquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
